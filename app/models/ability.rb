22e1d296da8ec296b60a7c36237fdb89d5636dca
Dalton A. Mitchell <daltonamitchell@live.com>
2015-12-14 22:38:41 +0000

    Added authorization

---
+++ app/models/ability.rb
@@ -0,0 +1,12 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the passed in user here.
+    user ||= User.new # guest user (not logged in)
+    can :read, :all
+    can :manage, Book do |book|
+      book.try(:user) == user
+    end
+  end
+end
